4b5f84a8caaf667b784d078977f0a747213d3191
Dhanraj Kushwah <dhanraj.kushwah007@gmail.com>
2024-07-31 12:41:12 +0530

    Dk

--- api_task/routes/index.js
+++ api_task/routes/index.js
@@ -3,3 +3,9 @@ const userRoute = require("../routes/user.route")
 const router = express.Router();
 router.use('/user', userRoute);
 module.exports = router;
+
+// Example API route
+router.get('/example', (req, res) => {
+  res.json({ message: 'This is an example API response' });
+});
+
